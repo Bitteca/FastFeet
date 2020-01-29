15a55822f9c7da54e6971a95aa3928ead4b1fa63
Bitteca <biteca@gmail.com>
2020-01-29 19:08:26 -0300

    Subindo codigo de adiçao de recipient, com bug

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import SessionController from './app/controllers/SessionController';
+import RecipientsController from './app/controllers/RecipientsController';
 
 import authMiddleware from './app/middleware/auth';
 
@@ -9,5 +10,6 @@ const routes = new Router();
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
+routes.post('/recipient', RecipientsController.store);
 
 export default routes;
