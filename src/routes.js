6581f8ed06122f44d1a6bb56c2134754b77815cd
Bitteca <biteca@gmail.com>
2020-01-29 17:45:29 -0300

    Adiconada autenticaçao do usuario

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middleware/auth';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  res.json({ message: 'Hello teste' });
-});
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
 
 export default routes;
